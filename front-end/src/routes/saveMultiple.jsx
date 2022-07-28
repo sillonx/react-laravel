169b1465349ebd326a0b7a4f58dec7f758e05610
sillonx <61588808+sillonx@users.noreply.github.com>
2022-07-28 12:37:16 +0200

    Edit

--- front-end/src/routes/saveMultiple.jsx
+++
@@ -1,9 +0,0 @@
-import { useRoutes } from 'react-router-dom';
-
-import PublicRoutes from './PublicRoutes';
-import GuestRoutes from './GuestRoutes';
-import AuthRoutes from './AuthRoutes';
-
-export default function MyRoutes() {
-  return useRoutes([PublicRoutes, GuestRoutes, AuthRoutes]);
-}
